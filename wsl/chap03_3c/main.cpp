25cfc94efff2f33a38494f045f2f7b5f4a33c151
H. Watanabe <kaityo@users.sourceforge.jp>
2021-07-06 20:54:29 +0900

    updates main.cpp

--- wsl/chap03_3c/main.cpp
+++ wsl/chap03_3c/main.cpp
@@ -8,11 +8,11 @@ extern "C" void KernelMain(uint64_t frame_buffer_base,
   for (uint64_t i = 0; i < frame_buffer_size; ++i) {
     frame_buffer[i] = 0;
   }
-  const int LX = 800;
+  const int frame_width = 800;
   const int size = 199;
   for (uint64_t x = 0; x < size; x++) {
     for (uint64_t y = 0; y < size; y++) {
-      uint64_t i = (x + LX * y) * 4;
+      uint64_t i = (x + frame_width * y) * 4;
       uint64_t j = (x + size * y) * 3;
       frame_buffer[i] = array[j + 2];
       frame_buffer[i + 1] = array[j + 1];
